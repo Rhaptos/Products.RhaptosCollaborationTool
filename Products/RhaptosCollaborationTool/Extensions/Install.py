945a54c6492ad676ab5385ceac63cad1e8e578e2
Josh LaPlace <josh@cnx.rice.edu>
2009-09-23 22:28:45 +0000

    CONNEXIONSSRV-34: Refactor to use GenericSetup

---
+++ Products/RhaptosCollaborationTool/Extensions/Install.py
@@ -0,0 +1,11 @@
+
+from Products.CMFCore.utils import getToolByName
+
+def install(portal):
+    portal_setup = getToolByName(portal, 'portal_setup')
+    import_context = portal_setup.getImportContextID()
+    portal_setup.setImportContext(
+            'profile-Products.RhaptosCollaborationTool:default')
+    portal_setup.runAllImportSteps()
+    portal_setup.setImportContext(import_context)
+
